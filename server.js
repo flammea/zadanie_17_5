696cce1ad3c36ec7650799d66275c1be4a19b59d
Beata Markunas <beata.markunas@gmail.com>
2018-05-06 19:49:25 +0200

    Add Middleware

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('Hello world');
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store.');
+    next();
 });
 
-app.use(function(req, res){
-    console.log('Hej, jestem pośrednikiem między żądaniem a odpowiedzią!');
+app.get('/', function(req, res) {
+    res.send('Hello world! --> Bez informacji o pośredniku przy żądaniu');
 });
+
+app.get('/store', function(req, res) {
+    res.send('To jest sklep');
+});
+
+
+
+
 var server = app.listen(3000, function() {
     console.log('Przykładowa aplikacja nasłuchuje na http://localhost:3000');
 });
